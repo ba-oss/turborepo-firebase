692ac293c55213fe06a772ed8dc5448176a4dfa4
Benrandja Akram <a.benrandja@gmail.com>
2022-12-28 15:49:26 +0100

    add docs to @shared/core

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,11 +1,21 @@
 import { sum } from "lodash-es";
 
-// sort values
+/**
+ * Calculates the sum of an array.
+ *
+ * @param values array of number to calculate the sum for.
+ * @returns the sum of the values.
+ */
 function sumValues(values: number[]) {
   return sum(values);
 }
 
-// program to display fibonacci sequence using recursion
+/**
+ * Calculates the fibonnaci of a number.
+ *
+ * @param num the number to calculate the fibonnaci of.
+ * @returns the fibonacci result if `num` is > 2.
+ */
 function fibonacci(num: number): number {
   if (num < 2) {
     return num;
